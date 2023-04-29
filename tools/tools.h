8609cda32530b5892c39cc2a26d23d0e9489d512
oswaldoooo <oswaldohome007@gmail.com>
2023-04-29 20:16:34 +0800

    update tools

--- tools/tools.h
+++ tools/tools.h
@@ -28,5 +28,39 @@ namespace ctools
     void wordcount(const char *src,const std::map<char,none> &delim,std::map<std::string,int> &output);
     //spilt the string
     std::vector<std::string> spilt(const char *origin,const char *delim);
+    template<class T>
+    class pointerplus{
+    private:
+        T* core;
+        bool needfree;
+        bool isarr=false;
+    public:
+        pointerplus()=delete;
+        pointerplus(T* newcore,bool needfree_=false);
+        pointerplus(T* newcore,bool isarre,bool needfree_=false);
+        ~pointerplus();
+        T* load();
+    };
+    template<class T>
+    pointerplus<T>::pointerplus(T* newcore,bool needfree_){
+        needfree=needfree_;
+        core=newcore;
+    }
+    template<class T>
+    pointerplus<T>::pointerplus(T* newcore,bool isarre,bool needfree_){
+        isarr=isarre;
+        needfree=needfree_;
+        core=newcore;
+    }
+    template<class T>
+    pointerplus<T>::~pointerplus(){
+        if(needfree){
+            if(isarr){
+                delete[] core;
+            }else{
+                delete core;
+            }
+        }
+    }
 } // namespace ctools
 #endif

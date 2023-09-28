df7a5922c5a4e839aca3882455885a940bbffac9
Oswaldoooo! <oswaldohome007@gmail.com>
2023-09-28 17:25:46 +0800

    add atoi and verify function for integer and ip address

--- stdlib/core.c
+++ stdlib/core.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h>
+#include <stdbool.h>
 int print(FILE* fl, const char* src, const char* filename, const int line, const char* funname)
 {
     if (src == NULL || strlen(src) == 0) return -1;
@@ -61,4 +62,67 @@ const char* formatInt(int data)
         memmove(global_mem, ti, level);
     }
     return global_mem;
+}
+
+bool verify(const char* src, unsigned short types_info)
+{
+    switch (types_info) {
+    case INTEGER: {
+        int tians;
+        return Atoi(src, &tians);
+    } break;
+    case UINTEGER: {
+        int tians;
+        return (Atoi(src, &tians) && tians >= 0);
+    } break;
+    case BOOLEAN: {
+        return (!strncmp(src, "true", 4) || !strncmp(src, "false", 5) || (strlen(src) == 1) && (src[0] == '1' || src[0] == '0'));
+    } break;
+    case IPADDR: {
+        unsigned short pos = 0;
+        unsigned short tival = 0;
+        unsigned short total = 0;
+        if (strlen(src) < 7 || strlen(src) > 15) return false;
+        for (size_t i = 0; i < strlen(src); i++) {
+            if (src[i] == '.') {
+                pos = 0;
+                tival = 0;
+                total++;
+            } else if (src[i] >= '0' && src[i] <= '9' && pos < 3) {
+                pos++;
+                tival *= 10;
+                tival += src[i] - '0';
+                if (tival >= 256) return false;
+            } else {
+                return false;
+            }
+        }
+        return total == 3;
+    } break;
+    default:
+        return false;
+    }
+    return true;
+}
+bool Atoi(const char* src, int* dst)
+{
+    bool neg = false;
+    if (strlen(src) < 1) return 0;
+    if (src[0] == '+' && strlen(src) > 1) {
+        src++;
+    } else if (src[0] == '-' && strlen(src) > 1) {
+        src++;
+        neg = true;
+    }
+    *dst = 0;
+    for (size_t i = 0; i < strlen(src); i++) {
+        if (src[i] >= '0' && src[i] <= '9') {
+            (*dst) *= 10;
+            (*dst) += src[i] - '0';
+        } else {
+            return false;
+        }
+    }
+    if (neg) *dst = -(*dst);
+    return true;
 }

d959c66c2902e64e7a3c8a97fe4ee33fab562a70
Oswaldoooo! <oswaldohome007@gmail.com>
2023-10-16 14:16:57 +0800

    Update net.h

--- net/net.h
+++ net/net.h
@@ -3,6 +3,17 @@
 #define TCP 0x01
 #define UDP 0x02
 #define UNIX 0x03
+#include <pthread.h>
+#include <stddef.h>
+struct chan {
+    int core[2];
+    pthread_mutex_t mutex;
+    size_t size;
+};
+int newchan(struct chan* src, size_t size);
+int closechan(struct chan* src);
+int sendchan(struct chan* src, const void* v);
+int revchan(struct chan* src, void* dst);
 void transform(const char*, int[4]);
 
 #endif
